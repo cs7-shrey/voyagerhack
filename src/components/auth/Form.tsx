e3f0cb4c0ea243c2c0a9976d16c958624e8ba0c2
shrey <shrey@kali>
2025-01-31 07:28:51 +0530

    feat: added state, routes for authentication

---
+++ src/components/auth/Form.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { AuthType, useAuthStore } from "@/store/useAuthStore";
+import { signUp, login } from "@/store/useAuthStore";
+import { Link } from "react-router";
+
+interface InputProps {
+    label: string;
+    type: string;
+    id: string;
+    placeholder?: string;
+    value: string;
+    setValue: (value: string) => void;
+}
+
+const Input: React.FC<InputProps> = ({ label, type, id, placeholder, value, setValue }) => {
+    return (
+        <div className="flex flex-col">
+            <label htmlFor={id} className="text-xs font-bold">
+                {label}
+            </label>
+            <input
+                type={type}
+                id={id}
+                placeholder={placeholder}
+                required={true}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                className="block w-full rounded-xl bg-white px-3 py-2 text-base text-gray-900 border-2 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-accentForeground sm:text-sm/6"
+            />
+        </div>
+    );
+};
+
+interface FormProps {
+    type: AuthType;
+    typeText: string;
+}
+const Form: React.FC<FormProps> = ({ type, typeText}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    // let onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+    const { error } = useAuthStore();
+    const onSubmit = (e: React.FormEvent<HTMLButtonElement>) =>  {
+        e.preventDefault();
+        if (!email || !password) {
+            return;
+        }
+        if (type === AuthType.SIGNUP) {
+            signUp(name, email, password);
+        } else {
+            login(email, password);
+        }
+    }
+    return (
+        <div className="w-full h-full flex flex-col gap-3">
+            <h1 className="text-3xl pb-4 font-semibold font- text-center">
+                {typeText}
+            </h1>
+            <form className="flex flex-col gap-4 w-full">
+                <div className="flex flex-col gap-4">
+                    {type === AuthType.SIGNUP && <div>
+                        <Input
+                            label="Name"
+                            type="text"
+                            id="name"
+                            placeholder={"Enter name"}
+                            value={name}
+                            setValue={setName}
+                        />
+                    </div>}
+                    <div>
+                        <Input
+                            label="Email"
+                            type="email"
+                            id="email"
+                            placeholder={"Enter email address"}
+                            value={email}
+                            setValue={setEmail}
+                        />
+                    </div>
+                    <div>
+                        <Input
+                            label="Password"
+                            type="password"
+                            id="password"
+                            placeholder="Enter password"
+                            value={password}
+                            setValue={setPassword}
+                        />
+                    </div>
+                    <div>
+                        {error && <p className="text-red-500 text-sm">{error}</p>}
+                    </div>
+                    <div className="mx-auto my-2 w-full">
+                        <button className="bg-accent text-white p-2 rounded-md w-full" onClick={onSubmit}>
+                            {typeText}
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <div className="text-center">
+                {type === AuthType.SIGNUP ? (
+                    <p>
+                        Already have an account?{" "}
+                        <Link to="/login" className="text-accent underline">
+                            Login
+                        </Link>
+                    </p>
+                ) : (
+                    <p>
+                        Don't have an account?{" "}
+                        <Link to="/signup" className="text-accent underline">
+                            Sign up
+                        </Link>
+                    </p>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Form;
---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,140 @@
+import { create } from 'zustand';
+import { axiosInstance } from '@/lib/axiosConfig';
+import { AxiosError, isAxiosError } from 'axios';
+import toast from 'react-hot-toast';
+interface AuthStore {
+    authUserEmail: string | null;
+    isSigningUp: boolean;
+    isLoggingIn: boolean;
+    isCheckingAuth: boolean;
+    error: string | null;
+    ranOnce: boolean;
+    setAuthUserEmail: (email: string | null) => void;
+    setIsSigningUp: (isSigningUp: boolean) => void;
+    setIsLoggingIn: (isLoggingIn: boolean) => void;
+    setIsCheckingAuth: (isCheckingAuth: boolean) => void;
+    setError: (error: string | null) => void;
+    setRanOnce: (ranOnce: boolean) => void;
+}
+
+interface ErrorResponse {
+    detail: string;
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({  
+    authUserEmail: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isCheckingAuth: false,
+    error: null,
+    ranOnce: false,
+    setAuthUserEmail: (email) => set({ authUserEmail: email }),
+    setIsSigningUp: (isSigningUp) => set({ isSigningUp }),
+    setIsLoggingIn: (isLoggingIn) => set({ isLoggingIn }),
+    setIsCheckingAuth: (isCheckingAuth) => set({ isCheckingAuth }),
+    setError:  (error) => set({ error }),
+    setRanOnce: (ranOnce) => set({ ranOnce }),
+}))
+
+// useAuthStore.subscribe
+
+
+export async function signUp(name: string, email: string, password: string) {
+    const { setIsSigningUp, setError, setAuthUserEmail } = useAuthStore.getState();
+    console.log('signing up')
+    try {
+        setIsSigningUp(true);
+        const response = await axiosInstance.post('/users/signup', { name, email, password });
+        if (response.status !== 200) {
+            setError(response.data.detail)
+            setAuthUserEmail(null);
+            throw new Error('Failed to sign up');
+        }
+        setAuthUserEmail(email);
+    }
+    // check if error is of type AxiosError
+    catch (error) {
+        if (isAxiosError(error)) {
+            const e = error as AxiosError;
+                const errorMessage = (e.response?.data as ErrorResponse)?.detail || 'Failed to sign up';
+                // const errorMessage = e.response?.data?.detail as string|| 'Failed to sign up';
+                setError(errorMessage);
+                // const ex = new AxiosError();
+        }
+        console.error(error);
+        
+    }
+    finally {
+        setIsSigningUp(false);
+    }
+}
+
+export async function login(email: string, password: string) {
+    const { setIsLoggingIn, setError, setAuthUserEmail } = useAuthStore.getState();
+    console.log('logging in')
+    try {
+        setIsLoggingIn(true);
+        const response = await axiosInstance.post('/users/login', { email, password });
+        if (response.status !== 200) {
+            setError(response.data.detail)
+            setAuthUserEmail(null);
+            throw new Error('Failed to log in');
+        }
+        setAuthUserEmail(email);
+        console.log('successfully logged in')
+    }
+    catch (error) {
+        if (isAxiosError(error)) {
+            const e = error as AxiosError;
+                const errorMessage = (e.response?.data as ErrorResponse)?.detail || 'Failed to sign up';
+                // const errorMessage = e.response?.data?.detail as string|| 'Failed to sign up';
+                setError(errorMessage);
+                // const ex = new AxiosError();
+        }
+        console.error(error);
+    }
+    finally {
+        setIsLoggingIn(false);
+    }
+}
+
+export async function checkAuth() {
+    const { setIsCheckingAuth, setAuthUserEmail } = useAuthStore.getState();
+    console.log('checking auth')
+    try {
+        setIsCheckingAuth(true);
+        const response = await axiosInstance.get('/users/checkauth');
+        if (response.status !== 200) {
+            throw new Error('Failed to check auth');
+        }
+        console.log(response.data.email);
+        setAuthUserEmail(response.data.email);
+    }
+    catch (error) {
+        console.error(error);
+    }
+    finally {
+        setIsCheckingAuth(false);
+    }
+}
+
+export async function logout() {
+    const { setAuthUserEmail } = useAuthStore.getState();
+    // setAuthUserEmail(null);
+    try {
+        const response = await axiosInstance.post('/users/logout');
+        if (response.status !== 200) {
+            throw new Error('Failed to log out');
+        }
+        setAuthUserEmail(null);
+    }
+    catch (error) {
+        toast.error("Failed to log out");
+        console.error(error);
+    }
+}
+
+export enum AuthType {
+    LOGIN = "login",
+    SIGNUP = "signup",
+}
