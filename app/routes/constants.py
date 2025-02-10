74a346949203073f6e7b87ef48ad8ec72cc90f14
shrey <sshrey0007@gmail.com>
2025-02-10 22:51:59 +0530

    fix: minor changes

--- app/routes/constants.py
+++ app/routes/constants.py
@@ -6,7 +6,7 @@ from app import schemas
 
 router = APIRouter(prefix="/constants", tags=["constants"])
 
-@router.get("/{type}/", response_model=list[schemas.Constants])
+@router.get("/{type}", response_model=list[schemas.Constants])
 def get_constants(type: str, db: Session = Depends(get_db)):
     if type == "property_type":
         db_results = db.query(models.Hotel.property_type.label("name")).distinct().all()
@@ -18,5 +18,8 @@ def get_constants(type: str, db: Session = Depends(get_db)):
             "room_amenity": models.RoomAmenity
         }
         db_results = db.query(mapping[type].name, mapping[type].code).all()
+        
+    if not db_results:
+        return []
     
     return db_results
