4d65add30c0531e8c9b42fff1e0293401d07dcb8
shrey <sshrey0007@gmail.com>
2025-02-12 06:39:51 +0530

    add: backend routes for booking hotel

--- app/schemas.py
+++ app/schemas.py
@@ -136,3 +136,11 @@ class BookHotelSchema(BaseModel):
     hotel_id: str
     room_type_id: int
     rate_plan_id: int
+    
+class BookingDetails(BaseModel):
+    booking_id: int
+    check_in: date
+    check_out: date
+    hotel: dict
+    room_type: dict
+    rate_plan: dict
